d07814509c43cd0a9b974353908b4d4f83502354
Gurleen Singh <grlnsngh@gmail.com>
2022-01-11 06:16:51 -0500

    added controller for display players

---
+++ controller/DisplayPlayers.php
@@ -0,0 +1,20 @@
+<?php 
+
+require_once('../model/IDisplayPlayer.php');
+require_once('../formatters/playerFormatters/PlayerFormatArray.php');
+require_once('../formatters/playerFormatters/PlayerFormatJson.php');
+require_once('../formatters/playerFormatters/PlayerFormatFile.php');
+
+
+class DisplayPlayers implements IDisplayPlayers {    
+
+    function displayPlayer($isCLI, $source) {
+
+        $players = $source->readPlayers();
+        
+        if(gettype($players)=='string'){
+            $players = json_decode($players);
+        }
+
+    }   
+}
